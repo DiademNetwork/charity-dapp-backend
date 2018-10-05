e998b62d656d839d9f0f3d695e432ca60335cefb
Unknown <sevenflash@yandex.ru>
2018-10-05 23:40:47 +0300

    microservice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
+const stream = require('getstream')
+const client = stream.connect(process.env.STREAM_KEY, process.env.STREAM_SECRET)
+const achievementsAggregated = client.feed(process.env.STREAM_ACHIEVEMENTS_AGGREGATED_GROUP, process.env.STREAM_ACHIEVEMENTS_AGGREGATED_FEED)
+const achievementsFlat = client.feed(process.env.STREAM_ACHIEVEMENTS_FLAT_GROUP, process.env.STREAM_ACHIEVEMENTS_FLAT_FEED)
+const transactionsFeed = client.feed(process.env.STREAM_TRANSACTIONS_GROUP, process.env.STREAM_TRANSACTIONS_FEED)
+
 const SenecaWeb = require('seneca-web')
 const Express = require('express')
 const Router = Express.Router
@@ -19,6 +25,9 @@ const service = require('seneca')({ debug: { undead: true } })
     adapter: require('seneca-web-adapter-express'),
     options: { parseBody: false }
   })
+  .use('basic')
+  .use('entity')
+  .use(require('./feeds'), { achievementsAggregated, achievementsFlat, transactionsFeed })
   .use(require('./achievements'))
   .use(require('./users'))
   .use(require('./networks/facebook'))
